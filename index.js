e41cec6d71b357b4a3d188c590fb6c4988d59748
Juan Pablo M. <pablochileidh@gmail.com>
2024-01-14 22:02:26 -0300

    inicio

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 precio = 400000
 
 precioSpan = document.querySelector(".precio-inicial");
+cantidadSpan = document.querySelector(".cantidad");
+totalSpan = document.querySelector(".valor-total");
+
 precioSpan.innerHTML = precio
