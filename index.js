5a3e98e7cf2d143d7ceba243aa7d5b17bb58c757
Juan Pablo M. <pablochileidh@gmail.com>
2024-01-14 22:10:34 -0300

    avances

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 precio = 500000;
+cantidad = 0;
 total = 0;
 
 precioSpan = document.querySelector(".precio-inicial");
@@ -6,4 +7,5 @@ cantidadSpan = document.querySelector(".cantidad");
 totalSpan = document.querySelector(".valor-total");
 
 precioSpan.innerHTML = precio;
+cantidadSpan.innerHTML = cantidad;
 totalSpan.innerHTML = total;
